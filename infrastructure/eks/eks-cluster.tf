db874c3e6c28834384edcf678bfcead8c736a77c
Sohail Mohammed <sohails101@gmail.com>
2023-06-19 16:13:00 -0400

    EKS Module

--- infrastructure/eks/eks-cluster.tf
+++ infrastructure/eks/eks-cluster.tf
@@ -1,73 +1,38 @@
-# EKS Cluster
-#  * Create the EKS cluster
-#  * Create the IAM role for the EKS cluster
-#  * Attach the necessary policies to the IAM role
-#  * Create a security group for the EKS cluster
-
 # Create the EKS cluster
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "sohtek-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-  vpc_config {
-    subnet_ids = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
-  }
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
 
-  tags = {
-    Name = "eks-cluster"
-  }
-}
+  cluster_name    = "sohtek"
+  cluster_version = "1.27"
 
-# Create the IAM role for the EKS cluster
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
+  cluster_endpoint_public_access = true
+  enable_irsa                    = true
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = aws_iam_role.ebs_csi_iam_role.arn
+      resolve_conflicts        = "OVERWRITE"
     }
-  ]
-}
-EOF
+  }
 
-  tags = {
-    Name = "eks-cluster-role"
+  vpc_id                   = aws_vpc.eks_vpc.id
+  subnet_ids               = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
+  control_plane_subnet_ids = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
+  eks_managed_node_group_defaults = {
+    instance_types = ["t2.large", "t2.small"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t2.large"]
+      capacity_type  = "SPOT"
+    }
   }
 }
 
-# Attach the necessary policies to the IAM role
-resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-# Create a security group for the EKS cluster
-resource "aws_security_group" "eks_cluster_sg" {
-  name        = "eks-cluster-sg"
-  description = "Security group for the EKS cluster"
-  vpc_id      = aws_vpc.eks_vpc.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "eks-cluster-sg"
-  }
-}
--- versions.tf
+++ versions.tf
@@ -1,8 +1,10 @@
+# Terraform Settings Block
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+      source = "hashicorp/aws"
+      version = ">= 4.65"
+     }
   }
 }
